972de69be19a6b9b1a72590ecc2a48d3305e2331
Brianlucca <brianlucca2002@gmail.com>
2024-04-03 11:46:28 -0300

    feat: added the input to search for participants and the title.

---
+++ src/components/attendee-list.tsx
@@ -0,0 +1,13 @@
+import { Search } from 'lucide-react'
+
+export function Attendee() {
+    return (
+        <div className="flex gap-3 items-center">
+            <h1 className="text-2xl font-bold">Participantes</h1>
+            <div className="px-3 w-72 py-1.5 border border-white/10 bg-transparent rounded-lg text-sm flex items-center gap-3">
+                <Search className='size-4 text-emerald-300' />
+                <input className="bg-transparent flex-1 outline-none" placeholder="Buscar Participante..." />
+            </div>
+        </div>
+    )
+}

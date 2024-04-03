3612449080aebb19df0e29894a742faa291da779
Brianlucca <brianlucca2002@gmail.com>
2024-04-03 11:09:09 -0300

    feat: added the header component code.

---
+++ src/components/header.tsx
@@ -0,0 +1,13 @@
+import nlwlogoicon from '../assets/nlw-unite-icon.svg'
+
+export function Header() {
+    return (
+        <div className='flex items-center gap-5 py-2'>
+            <img src={nlwlogoicon} />
+            <nav className='flex items-center gap-5'>
+                <a href='' className='font-medium text-sm text-zinc-300'>Eventos</a>
+                <a href='' className='font-medium text-sm'>Participantes</a>
+            </nav>
+        </div>
+    )
+}
